86a75c3a365df813c95b157e2e2bf946ea58fd1b
connor <schuchmannconnor@gmail.com>
2026-07-03 18:57:01 -0700

    Add seach_listings to query eBay Browse API

--- ebay_client.py
+++ ebay_client.py
@@ -16,8 +16,13 @@ TOKEN_URLS = {
     "PRODUCTION": "https://api.ebay.com/identity/v1/oauth2/token",
 }
 
+SEARCH_URLS = {
+    "SANDBOX": "https://api.sandbox.ebay.com/buy/browse/v1/item_summary/search",
+    "PRODUCTION": "https://api.ebay.com/buy/browse/v1/item_summary/search",
+}
+
 def get_access_token():
-    token_url = TOKEN_URLS[EBAY_ENV]         
+    token_url = TOKEN_URLS[EBAY_ENV] # env toggle         
 
     creds = f"{CLIENT_ID}:{CLIENT_SECRET}"
     creds_bytes = creds.encode()                  # txt -> bytes (for base64)
@@ -26,7 +31,7 @@ def get_access_token():
 
     headers = {
         "Content-Type": "application/x-www-form-urlencoded", # body is form data (not JSON)
-        "Authorization": f"Basic {creds_decoded}",           # user auth info
+        "Authorization": f"Basic {creds_decoded}",           # get token
     }
 
     data = {
@@ -35,14 +40,37 @@ def get_access_token():
     }
 
     response = requests.post(token_url, headers=headers, data=data) # send request
-    response.raise_for_status()                                     # guard for Ebay error
+    response.raise_for_status()                                     # guard for eBay error
     return response.json()["access_token"]                          # pull token -> JSON
 
-if __name__ == "__main__":
+def search_listings(token, query):
+    search_url = SEARCH_URLS[EBAY_ENV] # env toggle
 
+    headers = {
+        "Content-Type": "application/json", # body is form JSON (not data)
+        "Authorization": f"bearer {token}", # use token
+    }
+
+    params = {
+        "q": query,  # keyword
+        "limit": 50, # 0-200 per call
+    }
+
+    response = requests.get(search_url, headers=headers, params=params) # send request
+    response.raise_for_status()                                         # guard for eBay error
+    return response.json()                                              # pull info -> JSON
+
+
+if __name__ == "__main__":
+    # confirm env loaded
     print(f"Client ID: {CLIENT_ID}")
     print(f"Client Secret loaded: {bool(CLIENT_SECRET)}")
     print(f"Environment: {EBAY_ENV}")
-
+    
+    # confirm token
     token = get_access_token()
     print(f"Access token: {token[:15]}...")
+
+    results = search_listings(token, "nike shirt")
+    print(f"total matches: {results.get('total')}")
+    print(f"Items returned: {len(results.get('itemSummaries', []))}")
